62fdb149551f1db8b52965da65edddad62568726
Andrew Howard <andrew.bryce.howard@gmail.com>
2023-09-13 11:50:36 +0000

    cleaned up jukebox output

--- murph/jukebox.py
+++ murph/jukebox.py
@@ -1,14 +1,42 @@
 from lyricsgenius import Genius
 from cobe.brain import Brain
 from decouple import config
+import sys
+import re
+import pprint as pp
 
 genius = Genius(config('GENIUS_TOKEN'))
 
 genius.remove_section_headers = True
 genius.skip_non_songs = True
-genius.excluded_terms = ["(mix)", "(Live)", "(Demo)", "(Version)", "(DVD)", "(Edit)"]
+genius.excluded_terms = ["mix\)", "\(Live", "Demo\)", "Version\)", "DVD", "Edit\)"]
 
-artist = genius.search_artist('Muse')
+search_terms = sys.argv[1]
+search_max = 0 if len(sys.argv) != 3 else int(sys.argv[2])
+
+artist = genius.search_artist( search_terms, max_songs=search_max )
+
+lyrics_json = { (artist.name): list(
+    map( lambda s:
+        {
+            s.title:
+            re.sub( r'^.*Get tickets as low as.*$', '',
+                re.sub( r'^.*$', '',
+                    re.sub( r'[0-9]+Embed', '', s.lyrics ),
+                    1,
+                    re.MULTILINE
+                ),
+                0,
+                re.MULTILINE
+            )
+        },
+        artist.songs
+    )
+) }
+
+# print(lyrics_json)
+pp.pprint(lyrics_json)
+
 artist.save_lyrics()
 
 # The following command will produce the output we want, apart from the ads.
