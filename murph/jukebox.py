109c1ebc4b1013b672228a726ea3bf6ce690acd2
Andrew Howard <andrew.bryce.howard@gmail.com>
2023-09-15 22:34:54 +0000

    got the learning process working

--- murph/jukebox.py
+++ murph/jukebox.py
@@ -3,7 +3,8 @@ from cobe.brain import Brain
 from decouple import config
 import sys
 import re
-import pprint as pp
+import json
+import os
 
 genius = Genius(config('GENIUS_TOKEN'))
 
@@ -11,34 +12,57 @@ genius.remove_section_headers = True
 genius.skip_non_songs = True
 genius.excluded_terms = ["mix\)", "\(Live", "Demo\)", "Version\)", "DVD", "Edit\)"]
 
-search_terms = sys.argv[1]
-search_max = 0 if len(sys.argv) != 3 else int(sys.argv[2])
+arg_cnt = len(sys.argv)
+search_term = sys.argv[1]
 
-artist = genius.search_artist( search_terms, max_songs=search_max )
+if arg_cnt == 2: 
+    artist = genius.search_artist( search_term )
+elif arg_cnt == 3:
+    search_max = int( sys.argv[2] )
+    artist = genius.search_artist( search_term, max_songs = search_max )
+else: 
+    sys.exit('Arguments: search_artist(s) [max_songs] [sort_by]')
 
-lyrics_json = { (artist.name): list(
-    map( lambda s:
-        {
-            s.title:
-            re.sub( r'^.*Get tickets as low as.*$', '',
-                re.sub( r'^.*$', '',
-                    re.sub( r'[0-9]+Embed', '', s.lyrics ),
-                    1,
+# artist.save_lyrics( overwrite = True )
+
+songs = []
+for s in artist.songs:
+    songs.append( 
+        {   'title': s.title,
+            'lyrics': 
+                re.sub( r'^.*Get tickets as low as.*$', '',
+                    re.sub( r'^.*$', '',
+                        re.sub( r'[0-9]+Embed', '', 
+                            s.lyrics.replace( '\n\n', '\n' ).replace( '\n', '.\n' ) ),
+                        1,
+                        re.MULTILINE
+                    ),
+                    0,
                     re.MULTILINE
-                ),
-                0,
-                re.MULTILINE
-            )
-        },
-        artist.songs
+                )
+        }
     )
-) }
 
-# print(lyrics_json)
-pp.pprint(lyrics_json)
+artist_dict = { 'artist': artist.name, 'songs': songs }
 
-artist.save_lyrics()
+artist_json = json.dumps(artist_dict, indent=4)
 
-# The following command will produce the output we want, apart from the ads.
-# Maybe use ChatGPT to remove them?
-# jq '{ (.name): [.songs[] | select(.title | test("(?i)(live|mix|demo|version|DVD|edit)"; "i") | not) | {(.title): .lyrics}] }' Lyrics_Muse.json > muse_lyrics.json
+f = open( 'lyrics.json', 'w' )
+f.write( artist_json )
+f.close()
+
+output = ''
+for s in songs:
+    output += s.get('lyrics')
+
+# print( output )
+f = open( 'buffer.txt', 'w' )
+f.write( output )
+f.close()
+
+os.system( 'poetry run cobe -b brain.brn learn buffer.txt' )
+os.remove( 'buffer.txt' )
+
+brain = Brain( 'brain.brn' )
+reply = brain.reply( 'What''s black?' )
+print( reply )
