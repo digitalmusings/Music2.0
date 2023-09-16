f5d0cfc21449ee76208ad432315b3caa53c5fb12
Andrew Howard <andrew.bryce.howard@gmail.com>
2023-09-16 23:50:16 +0000

    simplified the settings

--- murph/murph.py
+++ murph/murph.py
@@ -5,22 +5,22 @@ from random import randint
 from cobe.brain import Brain
 from decouple import config
 
-settings["BOT_TOKEN"]             = config('BOT_TOKEN')
-settings["TRIGGER_ENABLED"]       = bool(config("TRIGGER_ENABLED"))
-settings["TRIGGER_WORDS"]         = [x.lower() for x in config("TRIGGER_WORDS").split(",")]
-settings["CHAT_ALLOWLIST"]        = config("CHAT_ALLOWLIST").split(",")
-settings["RANDOM_ENABLED"]        = bool(config("RANDOM_ENABLED"))
-settings["RANDOM_RATIO"]          = config("RANDOM_RATIO")
-settings["PRIVATE_REPLY_ENABLED"] = bool(config("PRIVATE_REPLY_ENABLED"))
-settings["BLOCKLIST_WORDS"]       = config("BLOCKLIST_WORDS").split(",")
-settings["LEARN_ENABLED"]         = bool(config("LEARN_ENABLED"))
+BOT_TOKEN               = config('BOT_TOKEN')
+TRIGGER_ENABLED         = bool(config("TRIGGER_ENABLED"))
+TRIGGER_WORDS           = [x.lower() for x in config("TRIGGER_WORDS").split(",")]
+CHAT_ALLOWLIST          = config("CHAT_ALLOWLIST").split(",")
+RANDOM_ENABLED          = bool(config("RANDOM_ENABLED"))
+RANDOM_RATIO            = config("RANDOM_RATIO")
+PRIVATE_REPLY_ENABLED   = bool(config("PRIVATE_REPLY_ENABLED"))
+BLOCKLIST_WORDS         = config("BLOCKLIST_WORDS").split(",")
+LEARN_ENABLED           = bool(config("LEARN_ENABLED"))
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-murph = Brain('murph.brain')
+brain = Brain('brain.brn')
 
 # UTILITY FUNCTIONS
 # Remove the first "trigger word" detected if string is beginning with it
@@ -32,7 +32,7 @@ def remove_trigger_words(string):
 
 # Return the "trigger word" the string is beginning with. None if not detected.
 def start_with_trigger_words(string):
-	for word in settings["TRIGGER_WORDS"]:
+	for word in TRIGGER_WORDS:
 		if string[:len(word)].lower() == word.lower():
 			return string[:len(word)]
 	return None
@@ -46,10 +46,10 @@ async def on_message(message):
     if message.author == client.user:
         return
     msg = message.content
-    if settings["LEARN_ENABLED"]:
-    		bender.learn(remove_trigger_words(msg))
-    if any(w in msg.lower() for w in settings["TRIGGER_WORDS"]) or (settings["RANDOM_ENABLED"] and randint(0, int(settings["RANDOM_RATIO"]))==0):
-    		reply = bender.reply(msg)
+    if LEARN_ENABLED:
+    		brain.learn(remove_trigger_words(msg))
+    if any(w in msg.lower() for w in TRIGGER_WORDS) or (RANDOM_ENABLED and randint(0, int(RANDOM_RATIO))==0):
+    		reply = brain.reply(msg)
     		await message.channel.send(reply)
 
-client.run(settings["BOT_TOKEN"])
+client.run(BOT_TOKEN)
