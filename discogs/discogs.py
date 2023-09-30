090eddd42f81754206da9eb25ea03d802c365f4b
Andrew Howard <andrew.bryce.howard@gmail.com>
2023-09-30 13:34:50 -0400

    Back to decouple, after all.

--- discogs/discogs.py
+++ discogs/discogs.py
@@ -1,6 +1,6 @@
 import argparse
 import discogs_client
-import os
+from decouple import config
 
 # Parse arguments
 parser = argparse.ArgumentParser(
@@ -10,9 +10,8 @@ parser.add_argument("search", help="What to search for")
 parser.add_argument("--type", default="master", help="What type of thing to search for", choices=["master", "release", "artist"])
 args = parser.parse_args()
 
-
-disc = discogs_client.Client("#Music2.0/0.1")
-disc.set_consumer_key(os.environ.get("DISCOGS_KEY"), os.environ.get("DISCOGS_SECRET"))
+disc = discogs_client.Client("#Music2.0/0.1", user_token=config("DISCOGS_TOKEN"))
+# disc.set_consumer_key(os.environ.get("DISCOGS_KEY"), os.environ.get("DISCOGS_SECRET"))
 
 results = disc.search(args.search, type=args.type)
 
