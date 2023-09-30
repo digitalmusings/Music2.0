8127ff6160bbdd91cc1ba8a4dc23cc3b7d75bb70
Andrew Howard <andrew.bryce.howard@gmail.com>
2023-09-30 13:04:27 -0400

    decouple not importing

--- discogs/discogs.py
+++ discogs/discogs.py
@@ -1,23 +1,23 @@
 import argparse
 from decouple import config
 import discogs_client
-
+# import os
 
 # Parse arguments
 parser = argparse.ArgumentParser(
-    prog = 'Discogs',
-    description = 'Search for a release',
-    epilog = ''
+    prog="Discogs", description="Search for a release", epilog=""
 )
-parser.add_argument( 'search', help = 'What to search for' )
-parser.add_argument( 'type', 
-                     default = 'master', 
-                     help = 'What type of thing to search for' 
-)
+parser.add_argument("search", help="What to search for")
+parser.add_argument("--type", default="master", help="What type of thing to search for", choices=["master", "release", "artist"])
 args = parser.parse_args()
 
-disc = discogs_client.Client( '#Music2.0/0.1', user_token = config( 'DISCOGS' ) )
 
-results = disc.search( args.search, type = args.type )
+disc = discogs_client.Client("#Music2.0/0.1")
+disc.set_consumer_key(config("DISCOGS_KEY"), config("DISCOGS_SECRET"))
+# disc.set_consumer_key(os.environ.get("DISCOGS_KEY"), os.environ.get("DISCOGS_SECRET"))
 
-print( results[0] )
+results = disc.search(args.search, type=args.type)
+
+result = results[0]
+
+print(result)
